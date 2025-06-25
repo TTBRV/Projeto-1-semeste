2220c8f36cc7ea5c7c4a20940dea72d6f5f2c9aa
Arthur Pereira <arthurpsouza502@gmail.com>
2025-06-25 12:18:32 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let nome_cadastro = document.getElementById('nome_cadastro')
+let senha_cadastro = document.getElementById('senha_cadastro')
+let email_cadastro = document.getElementById('email_cadastro')
+let cadastrar = document.getElementById('cadastrar')
+
+let email_login = document.getElementById('email_cadastro')
+let senha_login = document.getElementById('senha_login')
+let logar = document.getElementById('logar')
+
+cadastrar.addEventListener('click',function(){
+    var nome =
+    console.log(nome)
+})
